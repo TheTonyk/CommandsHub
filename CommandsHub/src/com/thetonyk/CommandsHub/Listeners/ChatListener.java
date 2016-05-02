fdf8e8e101bbf8f43d5e002d90f7631347c4fcf6
TheTonyk <lebrun.noe@gmail.com>
2016-05-02 13:32:38 +0200

    Fixed Mentions Alerts.

--- CommandsHub/src/com/thetonyk/CommandsHub/Listeners/ChatListener.java
+++ CommandsHub/src/com/thetonyk/CommandsHub/Listeners/ChatListener.java
@@ -33,35 +33,13 @@ public class ChatListener implements Listener {
 		
 		for (int i = 0; i < receivers.length; i++) {
 			
-			if (PlayerUtils.getChatVisibility(receivers[i]) == 0) {
-				
-				event.getRecipients().remove(receivers[i]);
-				
-			}
+			if (PlayerUtils.getChatVisibility(receivers[i]) == 0) event.getRecipients().remove(receivers[i]);
 			
 			if (PlayerUtils.getChatVisibility(receivers[i]) == 1 && event.getMessage().contains(receivers[i].getName()) && PlayerUtils.getMentionsState(receivers[i]) == 1) {
 				
-				if (event.getRecipients().contains(receivers[i])) {
-					
-					event.getRecipients().remove(receivers[i]);
-					
-				}
+				if (event.getRecipients().contains(receivers[i])) event.getRecipients().remove(receivers[i]);
 				
-				StringBuilder message = new StringBuilder();
-				
-				for (int j = 0; j < event.getMessage().split(" ").length; j++) {
-					
-					if (event.getMessage().split(" ")[j].equalsIgnoreCase(receivers[i].getName())) {
-						
-						message.append("§a§l" + receivers[i].getName() + "§f ");
-						
-					} else {
-					
-						message.append(event.getMessage().split(" ")[j] + " ");
-						
-					}
-					
-				}
+				String message = event.getMessage().replaceAll(receivers[i].getName(), "§a§l" + receivers[i].getName() + "§r");
 				
 				receivers[i].sendMessage(PlayerUtils.getRank(player.getName()).getPrefix() + "§7" + player.getName() + " §8⫸ §f" + message);
 				receivers[i].playSound(receivers[i].getLocation(), Sound.ORB_PICKUP, 1, 1);
@@ -71,7 +49,6 @@ public class ChatListener implements Listener {
 		}
 		
 		event.setFormat(PlayerUtils.getRank(player.getName()).getPrefix() + "§7" + player.getName() + " §8⫸ §f%2$s");
-		
 		cooldown.add(player);
 		
 		new BukkitRunnable() {
